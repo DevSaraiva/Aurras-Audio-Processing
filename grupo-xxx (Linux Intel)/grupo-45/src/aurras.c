1a1218e097c8407c0ab55c9f65254555ab988a75
devSaraiva <bernardosaraiva1503@gmail.com>
2021-06-12 12:30:00 +0100

    leitura do file options e alocação em memoria

--- grupo-xxx (Linux Intel)/grupo-45/src/aurras.c
+++ grupo-xxx (Linux Intel)/grupo-45/src/aurras.c
@@ -7,7 +7,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 
-int main(char ** args, int argc){
+int main(int argc, char ** args){
 
     char buffer[1024];
     int bytesRead = 0;
--- grupo-xxx (Linux Intel)/grupo-45/src/aurrasd.c
+++ grupo-xxx (Linux Intel)/grupo-45/src/aurrasd.c
@@ -9,8 +9,8 @@
 
 typedef struct filtro{
     
-    char identificador[20];
-    char executavel[20];
+    char * identificador;
+    char * executavel;
     int tamFilaEspera;
     int emEspera;
 
@@ -18,35 +18,105 @@ typedef struct filtro{
 } filtroConfig;
 
 
-void criaConfigs(filtroConfig configs[]){
+int contaLinhas(int fd){
+    char buff;
+    int linhas = 0;
+    int bytesRead = 0;
+
+
+    while((bytesRead = read(fd, &buff, 1)) > 0){
+        if(buff == '\n') linhas++;
+    }
+    return linhas;
+}
+
+ssize_t readln(int fd, char *line, size_t size){
+    size_t size_Line = 0;
+    int barraN = 0;
+       
+
+    do{
+        read(fd,line + size_Line,1);
+        if(line[size_Line] != '\n' && line[size_Line] != '\0') {
+            size_Line += 1;
+        }
+        else{
+            barraN = 1;
+        }
+    }
+    while(size_Line < size-1 && !barraN);
+    line[size_Line] = '\0';
+
+
+    return size_Line;
+}
+
+void criaConfigs(filtroConfig configs[], int numFiltros){
     
+    char line[1024];
+    
+    int fdConfig  = open("../etc/aurrasd.conf", O_RDONLY, 0666);
+    
+
+    for(int i = 0; i < numFiltros; i++){
+
+        readln(fdConfig, line, 1024);
+        configs[i].identificador = strdup(strtok(line," "));
+        configs[i].executavel = strdup(strtok(NULL," "));
+        configs[i].tamFilaEspera = atoi(strtok(NULL," "));
+        
+    }
+    
+     
 }
 
 
-int main(char ** args, int argc){
+int main(int argc, char ** args){
     
+    setbuf(stdout,NULL);
 
-
+    unlink("/tmp/fifo");
 
     if(mkfifo("/tmp/fifo", 0666) == -1){
             perror("mkfifo");
     
     }
+    
+    int fdConfig = open("../etc/aurrasd.conf", O_RDONLY, 0666);
+     
 
-    int fd = open("/tmp/fifo", O_RDONLY);
-    int hold_fifo = open("/tmp/fifo",O_WRONLY);
-    int bytesRead = 0;
-    char buffer[1024];
+    //int fd = open("/tmp/fifo", O_RDONLY);
+  
+    //int hold_fifo = open("/tmp/fifo",O_WRONLY);
+    
+    //int bytesRead = 0;
+    
+    //char buffer[1024];
+    
+    
+    //Leitura da configuração
     
 
-        while((bytesRead = read(fd, buffer, 1024)) > 0) {
-            write(1, buffer, bytesRead);
+    int numFiltros = contaLinhas(fdConfig);
+   
+    filtroConfig configs[numFiltros];
+
+    criaConfigs(configs, numFiltros);
+
+    printf("%s",configs[0].identificador);
+
+
+    
+
+
+        //while((bytesRead = read(fd, buffer, 1024)) > 0) {
+        //    write(1, buffer, bytesRead);
             
-        }
+        //}
 
     
-    close(fd);
-    close(hold_fifo);
+    //close(fd);
+    //close(hold_fifo);
     return 0;
     
 }
